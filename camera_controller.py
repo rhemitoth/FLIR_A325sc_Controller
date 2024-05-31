f59e6c76d8cf4414fde4dd6309a80bbc2c5a47ce
Rhemi Toth <rhemitoth@gmail.com>
2024-05-31 21:35:23 +0200

    update cam controller

--- camera_controller.py
+++ camera_controller.py
@@ -1,16 +1,27 @@
-# Import libs
+#### Import libraries ####
 import telnetlib
 import ftplib
 import datetime
 import time
 import PySpin
 
-# Connect to camera
-flir_ip = '169.254.0.1'
-tn = telnetlib.Telnet(flir_ip)
+#### Connect to camera ####
+def establish_telnet_connection(cam_ip):
+	telnet_connection = telnetlib.Telnet(cam_ip)
+	return telnet_connection
 
-#Focus the camera
-tn.read_until(b'>')
-tn.write(b'rset .system.focus.autofull true\n')
-time.sleep(5)
+#### Focus the camera ####
+def focus(telnet_connection):
+	telnet_connection.read_until(b'>')
+	telnet_connection.write(b'rset .system.focus.autofull true\n') # telnet command to focus the camera
+	telnet_connection.sleep(5)
+
+def main():
+	flir_ip = '169.254.0.1' # Camera IP address
+	tn = establish_telnet_connection(flir_ip)
+	focus(tn)
+
+if __name__ == '__main__':
+	main()
+
 
