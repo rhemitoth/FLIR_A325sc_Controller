90510acd1077573da82c2df5101bc4bf61eae433
rhemi <rhemitoth@gmail.com>
2024-06-24 13:56:34 -0400

    mmmmmgmmmmm

--- FLIR_A325sc_Controller_Complete.py
+++ FLIR_A325sc_Controller_Complete.py
@@ -26,42 +26,42 @@ import time
 # to connect to the camera and grab an image
 
 def save_image_spinnaker(directory, filetype):
-	# Argument 'directory' specifies directory where image will be saved
-	# Argument 'filetype' specifies filetype of image to be saves ('png', 'jpg', 'tiff', etc)
-	
-	# Initalize the system
-	system = PySpin.System.GetInstance()
+    # Argument 'directory' specifies directory where image will be saved
+    # Argument 'filetype' specifies filetype of image to be saves ('png', 'jpg', 'tiff', etc)
+    
+    # Initalize the system
+    system = PySpin.System.GetInstance()
 
-	# Get the camera
-	cam = system.GetCameras()[0]
-	
-	try:
-		# Initialize the camera
-		cam.Init()
+    # Get the camera
+    cam = system.GetCameras()[0]
+    
+    try:
+        # Initialize the camera
+        cam.Init()
 
-		# Start aquisition
-		cam.BeginAcquisition()
+        # Start aquisition
+        cam.BeginAcquisition()
 
-		# Grab image
-		image_result = cam.GetNextImage()
-	
-		# Save image
-		filename = directory + 'file-' + str(datetime.datetime.now().strftime('%Y%m%d-%H%M%S')) + "." + filetype
-		if os.path.exists(directory):
-			image_result.Save(filename)
+        # Grab image
+        image_result = cam.GetNextImage()
+    
+        # Save image
+        filename = directory + 'file-' + str(datetime.datetime.now().strftime('%Y%m%d-%H%M%S')) + "." + filetype
+        if os.path.exists(directory):
+            image_result.Save(filename)
 
-		# Release image
-		image_result.Release()
+        # Release image
+        image_result.Release()
 
-		# Stop Acquisition
-		cam.EndAcquisition()
+        # Stop Acquisition
+        cam.EndAcquisition()
 
-		# Deinitalize camera
-		cam.DeInit()
+        # Deinitalize camera
+        cam.DeInit()
 
-	finally:
-		# Release system instance
-		system.ReleaseInstance()
+    finally:
+        # Release system instance
+        system.ReleaseInstance()
 
 
 #### Check for camera connection ####
@@ -70,48 +70,48 @@ def save_image_spinnaker(directory, filetype):
 # Returns 'True' if a camera is connected
 
 def check_connection():
-	# Initalize the system
-	system = PySpin.System.GetInstance()
+    # Initalize the system
+    system = PySpin.System.GetInstance()
 
-	# Get the list of connected cameras
-	cam_list = system.GetCameras()
+    # Get the list of connected cameras
+    cam_list = system.GetCameras()
 
-	# Get length of camera list
-	numcams = len(cam_list)
+    # Get length of camera list
+    numcams = len(cam_list)
 
-	# Return true if the camera is connected. Return false is no camera is connected.
-	if numcams > 0:
-		return True
-	else:
-		return False
+    # Return true if the camera is connected. Return false is no camera is connected.
+    if numcams > 0:
+        return True
+    else:
+        return False
 
 #### Establishing Telnet Connection ####
 def establish_telnet_connection(cam_ip):
-	telnet_connection = telnetlib.Telnet(cam_ip)
-	return telnet_connection
+    telnet_connection = telnetlib.Telnet(cam_ip)
+    return telnet_connection
 
 #### Focusing the camera via telnet ####
 def focus(telnet_connection):
-	telnet_connection.read_until(b'>')
-	telnet_connection.write(b'rset .system.focus.autofull true\n') # telnet command to focus the camera
-	sleep(5)
+    telnet_connection.read_until(b'>')
+    telnet_connection.write(b'rset .system.focus.autofull true\n') # telnet command to focus the camera
+    sleep(5)
 
 #### Drawing an image on the e-ink display ####
 
 def print_to_display(deer_on = True, deer_path = "/home/moorcroftlab/Documents/FLIR/raspi_text_background.bmp", message = "hello deer",textX = 20,textY = 50,fontPath = "/usr/share/fonts/X11/Type1/NimbusMonoPS-Bold.pfb",fontSize = 20):
-	# This function is used to print out messages describing what the system is doing to the e-paper display
-	# deer_on = True, the messages will appear as text inside of a speech bubble coming from a cartoon of a roe deer (I'm using this system to capture images of roe deer and I thought it would be cute ¯\_(ツ)_/¯)
-	# deer_on = False, the message will appear as plain text 
-	# deer_path = location on your Pi where the deer image is saved
-	# message = "the text you want to display on the screen"
-	# textX = X position to draw text
-	# textY = Y position to draw text
-	# fontPath = Path to the font you want to use. You can get a list of paths of fonts installed on your Pi by typing "fc-list" in the terminal
+    # This function is used to print out messages describing what the system is doing to the e-paper display
+    # deer_on = True, the messages will appear as text inside of a speech bubble coming from a cartoon of a roe deer (I'm using this system to capture images of roe deer and I thought it would be cute ¯\_(ツ)_/¯)
+    # deer_on = False, the message will appear as plain text 
+    # deer_path = location on your Pi where the deer image is saved
+    # message = "the text you want to display on the screen"
+    # textX = X position to draw text
+    # textY = Y position to draw text
+    # fontPath = Path to the font you want to use. You can get a list of paths of fonts installed on your Pi by typing "fc-list" in the terminal
 
-	if deer_on == True:
-		epd = epd2in7_V2.EPD()
-		epd.init()
-		epd.Clear()
+    if deer_on == True:
+        epd = epd2in7_V2.EPD()
+        epd.init()
+        epd.Clear()
         
         #Create a blank image for drawing
         canvas = Image.new('1',(epd.width,epd.height),255)
@@ -147,7 +147,7 @@ def print_to_display(deer_on = True, deer_path = "/home/moorcroftlab/Documents/F
         # Display the image on the e-Paper display
         epd.display_Fast(epd.getbuffer(canvas))
     else:
-    	epd = epd2in7_V2.EPD()
+        epd = epd2in7_V2.EPD()
         epd.init()
         epd.Clear()
         
@@ -174,101 +174,101 @@ def print_to_display(deer_on = True, deer_path = "/home/moorcroftlab/Documents/F
 
 #### Clear the e-ink display #####
 def clear_display():
-	epd = epd2in7_V2.EPD()
+    epd = epd2in7_V2.EPD()
     epd.init()
     epd.Clear()
 
 #### Save pictures #####
 def collect_data(fpath = "/media/moorcroftlab/9016-4EF8/",duration = 30):
-	# f_path = "directory where images will be saved"
-	# duration = duration in minutes of data collection
-	start_time = time()
-	elapsed_time = 0
-	while elapsed_time < duration * 60: 
-		if os.path.exists(fpath) and check_connection() == True:
-			print("Capturing image . . .")
-			save_image_spinnaker(directory = fpath, filetype = "tiff")
-			print ("Image saved.")
-			# wait 10 seconds before taking the next picture
-			sleep(10)
-		elif os.path.exists(fpath) == False:
-			print("WARNING: SD Card missing.")
-			print_to_display("WARNING!!!\nNo SD card detected")
-			sd_missing = True
-			while sd_missing == True:
-				sd_missing = os.path.exists(fpath)
-				sleep(1)
-		elapsed_time = time() - start_time()
-	     
+    # f_path = "directory where images will be saved"
+    # duration = duration in minutes of data collection
+    start_time = time()
+    elapsed_time = 0
+    while elapsed_time < duration * 60: 
+        if os.path.exists(fpath) and check_connection() == True:
+            print("Capturing image . . .")
+            save_image_spinnaker(directory = fpath, filetype = "tiff")
+            print ("Image saved.")
+            # wait 10 seconds before taking the next picture
+            sleep(10)
+        elif os.path.exists(fpath) == False:
+            print("WARNING: SD Card missing.")
+            print_to_display("WARNING!!!\nNo SD card detected")
+            sd_missing = True
+            while sd_missing == True:
+                sd_missing = os.path.exists(fpath)
+                sleep(1)
+        elapsed_time = time() - start_time()
+         
 #### Main Code ####
 
 def main():
 
-	# Define PIR sensor GPIO pins on Raspberry Pi
-	pir = MotionSensor(24)
-	relay = gpiozero.OutputDevice(21, active_high = True, initial_value = False)
+    # Define PIR sensor GPIO pins on Raspberry Pi
+    pir = MotionSensor(24)
+    relay = gpiozero.OutputDevice(21, active_high = True, initial_value = False)
 
-	# Main code 
+    # Main code 
 
-	
-	while True:
-		# Check if motion is detected
-		
-		current_motion = pir.motion_detected
-		
-		if current_motion == True:
+    
+    while True:
+        # Check if motion is detected
+        
+        current_motion = pir.motion_detected
+        
+        if current_motion == True:
 
-			# Turn on camera
-			print("Motion detected. Turning on camera")
-			relay.on()
-			
-			# Pause code until camera is connected
-			print("Connecting to camera . . .")
-			print_to_display(message = "Connecting to camera . . .")
-			not_connected = True
-			start_time = time()
-			while not_connected == True:
-				connection_status = check_connection()
-				if connection_status == True:
-					not_connected = False
-				else:
-					current_time = time()
-					if current_time - start_time > 60:
-						print("Camera frozen. Restarting . . . ")
-						print_to_display(message = "Camera frozen.\nRestarting system . . .")
-						relay.off()
-						sleep(60)
-						relay.on()
-						print("Connecting to camera . . .")
-						print_to_display(message = "Connecting to camera . . .")
-						sleep(1)
-						start_time = time()
-					else:
-						sleep(1)
-			print("Camera connected.")
+            # Turn on camera
+            print("Motion detected. Turning on camera")
+            relay.on()
+            
+            # Pause code until camera is connected
+            print("Connecting to camera . . .")
+            print_to_display(message = "Connecting to camera . . .")
+            not_connected = True
+            start_time = time()
+            while not_connected == True:
+                connection_status = check_connection()
+                if connection_status == True:
+                    not_connected = False
+                else:
+                    current_time = time()
+                    if current_time - start_time > 60:
+                        print("Camera frozen. Restarting . . . ")
+                        print_to_display(message = "Camera frozen.\nRestarting system . . .")
+                        relay.off()
+                        sleep(60)
+                        relay.on()
+                        print("Connecting to camera . . .")
+                        print_to_display(message = "Connecting to camera . . .")
+                        sleep(1)
+                        start_time = time()
+                    else:
+                        sleep(1)
+            print("Camera connected.")
 
-			# Focus the camera
-			if check_connection() == True:
-				flir_ip = '169.254.0.2' 
-				tn = establish_telnet_connection(flir_ip)
-				print("Focusing camera . . .")
-				print_to_display(message = "Focusing camera . . .")
-				focus(tn)
-				print("Camera is focused.")
+            # Focus the camera
+            if check_connection() == True:
+                flir_ip = '169.254.0.2' 
+                tn = establish_telnet_connection(flir_ip)
+                print("Focusing camera . . .")
+                print_to_display(message = "Focusing camera . . .")
+                focus(tn)
+                print("Camera is focused.")
 
-			# Collect and image every 10 seconds for 30 minutes
-			print_to_display(message = "Capturing images . . .")
-			collect_data()
+            # Collect and image every 10 seconds for 30 minutes
+            print_to_display(message = "Capturing images . . .")
+            collect_data()
 
 
-		if current_motion == False:
-			relay.off() # turn off relay/power off camera
-			print("No motion detected. Camera off.")
-			print_to_display(message = "No motion detected.\nPowering down.")
-			sleep(10) # prevents the camera from freezing from turning on/off to quickly
-			clear_display()
-			
+        if current_motion == False:
+            relay.off() # turn off relay/power off camera
+            print("No motion detected. Camera off.")
+            print_to_display(message = "No motion detected.\nPowering down.")
+            sleep(10) # prevents the camera from freezing from turning on/off to quickly
+            clear_display()
+            
 if __name__ == '__main__':
-	main()
+    main()
 
 
